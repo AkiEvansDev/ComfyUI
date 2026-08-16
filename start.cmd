f649b63e14e98072eaf67dc7fc233b32d3cd0bdd
AkiEvansDev <aki.evans.dev@gmail.com>
2026-08-16 22:16:17 +0300

    up

--- start.cmd
+++ start.cmd
@@ -1,10 +1,7 @@
 @echo off
 cd /d "%~dp0"
 
-git pull
 call venv\Scripts\activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
 
 python main.py --disable-dynamic-vram --windows-standalone-build --port 8188 --front-end-version Comfy-Org/ComfyUI_frontend@latest
 pause
